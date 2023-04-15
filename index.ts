6e6e8616e9ea49040390e63aeffb08bee20fade5
Emissions <unitedrealm@hotmail.com>
2023-04-15 19:00:43 +0100

    updated exports

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
 import App from "./src/App.js";
 export default new App();
+
+export { IMessageData, IMessageEditData } from "./src/Types/Message.js";
+export { IPage } from "./src/Types/Paging.js";
