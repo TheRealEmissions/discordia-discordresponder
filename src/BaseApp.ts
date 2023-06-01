16a04d17787b157d186a2546ca751a9a60fec09e
Emissions <unitedrealm@hotmail.com>
2023-06-01 05:35:13 +0100

    updated to ada-events

--- src/BaseApp.ts
+++ src/BaseApp.ts
@@ -1,6 +1,6 @@
 import Base from "ts-modular-bot-file-design";
 import { Dependencies, Dependency } from "ada-types";
-import Events from "ts-modular-bot-addon-events-types";
+import Events from "ada-events-types";
 import DiscordClient from "ada-discordclient-types";
 
 abstract class BaseApp extends Base {
